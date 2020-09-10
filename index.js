6465ae6a733193c6bcc8a3930c3a49777355d3fe
Emmanuel Gumede <gumede.em@gmail.com>
2020-09-10 23:26:39 +0200

    login page design

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const exphbs = require("express-handlebars");
 
 const app = express();
 
+app.use(express.static("public"));
 app.engine("hbs", exphbs({ defaultLayout: "main", extname: "hbs" }));
 app.set("view engine", "hbs");
 
