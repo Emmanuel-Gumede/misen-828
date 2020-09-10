95468a8625274701720a28b17de06bdc6508e80e
Emmanuel Gumede <gumede.em@gmail.com>
2020-09-10 20:34:53 +0200

    add view engine and login page

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 const express = require("express");
+const exphbs = require("express-handlebars");
+
 const app = express();
 
+app.engine("hbs", exphbs({ defaultLayout: "main", extname: "hbs" }));
+app.set("view engine", "hbs");
+
 app.get("/", (req, res) => {
-	return res.send("This is my new applications ... more to come");
+	return res.render("login");
 });
 
 app.listen(4040, () => {
